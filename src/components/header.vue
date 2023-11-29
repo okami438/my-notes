09225f9fe63d03bd1e4a7dbb4ec53b3be4d15e24
Сергей Сидоренко <sidorenko.s@routeamgroup.com>
2023-11-29 11:50:11 +0300

    Initial commit

---
+++ src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <img src="/src/assets/images/logo.svg" alt="SVG">
+    <button class="button-enter">
+      <img src="/src/assets/images/Vector.svg" alt="SVG">
+      <span class="text-normal" :style="{color: 'var(--white)'}">Вход</span>
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<style scoped lang="scss">
+
+header {
+  display: flex;
+  width: auto;
+  padding: var(--40, 40px) var(--160, 160px);
+  justify-content: space-between;
+  align-items: center;
+  border-radius: var(--0, 0px);
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <HelloWorld/>
+    <main class="main">
+      <section class="main__left-side">
+        <h1 :style="{color: 'var(--white)'}">Мои заметки</h1>
+        <h3 :style="{color: 'var(--gray)'}">Не забывай о важном, храни его в облаке.</h3>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/header.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+.main {
+  display: flex;
+  padding: var(--0, 0px) var(--80, 80px);
+  border-radius: var(--0, 0px);
+  justify-content: space-between;
+  align-items: center;
+  flex: 1 0 0;
+  align-self: stretch;
+
+
+  &__left-side {
+    display: flex;
+    height: 392px;
+    padding: var(--0, 0px);
+    flex-direction: column;
+    align-items: flex-start;
+    gap: var(--40, 40px);
+    flex: 1 0 0;
+  }
+}
+
+
+</style>
