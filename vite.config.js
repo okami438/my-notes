698046955a39204b293a9f5e4dc7329fd0ffab1c
okami438 <sidorenko.s@routeamgroup.com>
2023-12-04 17:01:53 +0300

    master. Редактирование yml файла, чистка deploy файла от Vite сборщика, вернул сертификат

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 import mkcert from 'vite-plugin-mkcert'
-import vue from '@vitejs/plugin-vue2';
 import path from 'path';
 
 
 export default defineConfig({
     plugins: [
-        // createVuePlugin(),
-        vue(),
+        createVuePlugin(),
         mkcert()
     ],
     server: {
         cors: true,
         port: 8080,
-        // https: true,
+        https: true,
         //TODO: Если вдруг снова переведут на куки - раскоментировать
         // proxy: {
         //     "/api": {
